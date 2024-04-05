3b768dec3eaddead013ee3a5fc6a1aed6dcd0474
Sitara Rezaee <122310551+sitararez@users.noreply.github.com>
2024-04-05 10:17:45 -0400

    Add files via upload

---
+++ CSS/styles.js
@@ -0,0 +1,275 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	searchBarImg: {
+		width: 140,
+		height: 140,
+	},
+
+	searchBarContainer: {
+		// marginTop: 20,
+		marginRight: 30,
+	},
+
+	messageImg: {
+		width: 60,
+		height: 60,
+	},
+
+	messageContainer: {
+		// marginTop: 20,
+		marginLeft: 30,
+	},
+
+	bottomButtons: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		backgroundColor: "pink",
+		width: "100%",
+		top: 250,
+	},
+
+	text: {
+		fontSize: 40,
+		justifyContent: "center",
+		alignItems: "center",
+		alignContent: "center",
+	},
+
+	textContainer: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	logoImg: {
+		width: 45,
+		height: 45,
+		alignContent: "center",
+		justifyContent: "center",
+	},
+
+	logoContainer: {
+		alignItems: "center",
+		marginBottom: 10,
+	},
+
+	welcomeBack: {
+		marginTop: 150,
+		position: "relative",
+	},
+
+	gyb: {
+		fontSize: 20,
+	},
+
+	viewAllButton: {
+		backgroundColor: "#D2E6BE",
+		marginRight: 30,
+		marginTop: 150,
+		paddingTop: 10,
+		paddingBottom: 10,
+		paddingHorizontal: 10,
+		borderRadius: 10,
+	},
+
+	notifs: {
+		flexDirection: "row",
+		// alignItems: "center",
+		justifyContent: "space-between",
+	},
+
+	viewAllText: {
+		fontSize: 18,
+	},
+
+	notificationText: {
+		fontSize: 28,
+	},
+
+	notificationContainer: {
+		marginLeft: 30,
+		marginTop: 150,
+	},
+
+	notificationButtons: {
+		backgroundColor: "#DEF1CA",
+		marginHorizontal: 30,
+		borderRadius: 10,
+		paddingTop: 10,
+		paddingBottom: 15,
+	},
+
+	notificationsData: {
+		flexDirection: "column",
+	},
+
+	notificationsContainer: {
+		flexDirection: "column",
+		justifyContent: "space-between",
+		marginTop: 25,
+	},
+
+	notificationTexts: {
+		marginLeft: 20,
+	},
+
+	navBar: {
+		flexDirection: "row",
+		// backgroundColor: "#CEE4B9",
+		backgroundColor: "#ffffff",
+		// position: "absolute",
+		// height: "15%",
+		// flexDirection: "row",
+
+		justifyContent: "space-between",
+		height: "40%",
+		bottom: 55,
+	},
+
+	navBarPosition: {
+		flex: 1,
+		alignContent: "center",
+		width: "100%",
+	},
+
+	hamburgerImg: {
+		position: "relative",
+		width: 45,
+		height: 45,
+	},
+
+	logoImg: {
+		width: 45,
+		height: 45,
+	},
+
+	hamburger: {
+		position: "relative",
+		marginTop: 2,
+		marginRight: 30,
+	},
+
+	logo: {
+		marginTop: 1,
+		marginLeft: 30,
+	},
+
+	background: {
+		display: "flex",
+		backgroundColor: "#E8F8D8",
+		flex: 1,
+	},
+
+	headerProfitsPage: {
+		fontSize: 30,
+		marginHorizontal: "5%",
+	},
+
+	header2ProfitsPage: {
+		fontSize: 16,
+		marginHorizontal: "7%",
+		textAlign: "justify",
+	},
+
+	submitButton: {
+		backgroundColor: "#C6DCB0",
+		marginRight: 30,
+		paddingTop: 10,
+		paddingBottom: 10,
+		paddingHorizontal: 10,
+		borderRadius: 10,
+	},
+
+	addProfitsSquare: {
+		backgroundColor: "red",
+		paddingTop: 30,
+		paddingBottom: 30,
+		width: 200,
+		flexDirection: "column",
+		alignItems: "center",
+		borderRadius: 15,
+	},
+
+	submitDropdownContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+
+	submitButtonContainer: {
+		marginLeft: 110,
+	},
+
+	textForAdd: {
+		textAlign: "left",
+	},
+
+	menuContainer: {
+		position: "absolute",
+		marginLeft: "68%",
+		zIndex: 2,
+		marginTop: -185,
+	},
+
+	menuItem: {
+		fontSize: 18,
+		marginVertical: 15,
+	},
+
+	menuText: {
+		textAlign: "center",
+		position: "absolute",
+	},
+
+	visible: {
+		display: "flex",
+	},
+
+	submitText: {
+		color: "#fff",
+		fontSize: 16,
+		textAlign: "center",
+	},
+
+	addProfitsPosition: {
+		flex: 1,
+		alignContent: "center",
+		justifyContent: "center",
+		backgroundColor: "blue",
+	},
+
+	flexAddProfits: {
+		flex: 1,
+		justifyContent: "center",
+		alignContent: "center",
+		alignItems: "center",
+		backgroundColor: "#E8F2DE",
+	},
+
+	menuPosition: {
+		flex: 1,
+		position: "absolute",
+		width: 200,
+		height: 20,
+		backgroundColor: "orange",
+	},
+
+	addDebtsContainer: {
+		flex: 1,
+	},
+
+	label: {
+		fontSize: 16,
+		fontWeight: "bold",
+		color: "black",
+		marginTop: "5%",
+	},
+
+	profitsListButtons: {
+		backgroundColor: "#DEF1CA",
+		paddingVertical: "5%",
+		borderRadius: 10,
+		width: "85%",
+		marginVertical: 10,
+	},
+});
