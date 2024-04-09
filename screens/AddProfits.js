a0eccc26c3f9448912683b9c1e6206a70842f1af
Sitara Rezaee <122310551+sitararez@users.noreply.github.com>
2024-04-09 09:50:57 -0400

    Add files via upload

---
+++ screens/AddProfits.js
@@ -0,0 +1,88 @@
+import { React, useState } from "react";
+import { TouchableOpacity, View, Text, ScrollView } from "react-native";
+import { styles } from "../CSS/styles";
+import DropdownMonth from "../components/DropdownMonth";
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer";
+import MenuOptions from "../components/MenuOptions";
+import { useNavigation } from "@react-navigation/native";
+
+const AddProfits = () => {
+	const navigation = useNavigation();
+
+	const navigateToYearlyProfitsWithData = () => {
+		navigation.navigate("YearlyProfitsWithData");
+	};
+
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const openMenu = () => {
+		setMenuOpen(true);
+	};
+
+	const closeMenu = () => {
+		setMenuOpen(false);
+	};
+	return (
+		<View
+			style={{
+				display: "flex",
+				flex: 1,
+				justifyContent: "space-evenly",
+				backgroundColor: "#E8F8D8",
+			}}
+		>
+			<NavigationBar openMenu={openMenu} />
+
+			<MenuOptions visible={menuOpen} closeMenu={closeMenu} />
+
+			<ScrollView style={{ top: "25%" }}>
+				<View
+					style={{
+						display: "flex",
+						alignItems: "center",
+						alignContent: "center",
+					}}
+				>
+					<View
+						style={{
+							display: "flex",
+							backgroundColor: "#fff",
+							justifyContent: "space-between",
+							alignContent: "center",
+							width: "90%",
+							paddingTop: "5%",
+							borderRadius: 10,
+						}}
+					>
+						<View
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<Text style={{ marginLeft: "7%", fontSize: 16 }}>
+								Add:
+							</Text>
+							<TouchableOpacity
+								style={styles.submitButton}
+								onPress={navigateToYearlyProfitsWithData}
+							>
+								<Text style={styles.submitText}>Submit!</Text>
+							</TouchableOpacity>
+						</View>
+
+						<DropdownMonth />
+					</View>
+				</View>
+
+				<View style={{ height: "7%" }}></View>
+			</ScrollView>
+
+			<Footer />
+		</View>
+	);
+};
+
+export default AddProfits;
---
+++ screens/ProfitTracking.js
@@ -0,0 +1,124 @@
+import { React, useState } from "react";
+import { styles } from "../CSS/styles";
+import { View, Text, TouchableOpacity, ScrollView, Button } from "react-native";
+import DropdownYear from "../components/DropdownYear";
+import { useNavigation } from "@react-navigation/native";
+import MenuOptions from "../components/MenuOptions";
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer";
+
+const ProfitTracking = () => {
+	const navigation = useNavigation();
+
+	const navigateToYearlyProfits = () => {
+		navigation.navigate("YearlyProfits");
+	};
+
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const openMenu = () => {
+		setMenuOpen(true);
+	};
+
+	const closeMenu = () => {
+		setMenuOpen(false);
+	};
+
+	return (
+		<View style={{ backgroundColor: "#E8F8D8", display: "flex", flex: 1 }}>
+			<NavigationBar openMenu={openMenu} />
+			<MenuOptions visible={menuOpen} closeMenu={closeMenu} />
+			<ScrollView style={{ display: "flex", flex: 1 }}>
+				<View
+					style={{
+						display: "flex",
+						top: "5%",
+					}}
+				>
+					<View
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							justifyContent: "space-between",
+						}}
+					>
+						<Text style={styles.headerProfitsPage}>
+							Profit Tracking
+						</Text>
+						<TouchableOpacity
+							style={styles.submitButton}
+							onPress={navigateToYearlyProfits}
+						>
+							<Text style={styles.submitText}>Submit!</Text>
+						</TouchableOpacity>
+					</View>
+
+					<Text style={styles.header2ProfitsPage}>
+						Select year to generate profits list.
+					</Text>
+					<DropdownYear />
+					<View style={{ display: "flex", height: "10%" }}></View>
+				</View>
+				<View style={{}}>
+					<Text style={styles.headerProfitsPage}>
+						Existing Lists:
+					</Text>
+					<View
+						style={{
+							display: "flex",
+							width: "100%",
+							flexDirection: "column",
+							alignItems: "center",
+							justifyContent: "space-between",
+						}}
+					>
+						<TouchableOpacity style={styles.profitsListButtons}>
+							<View
+								style={{
+									display: "flex",
+									flexDirection: "column",
+									justifyContent: "space-evenly",
+									alignItems: "flex-start",
+								}}
+							>
+								<Text style={{ left: "5%" }}>
+									{" "}
+									2023 | 12 items
+								</Text>
+								<View
+									style={{
+										backgroundColor: "#DEF1CA",
+										height: "5%",
+									}}
+								></View>
+								<Text style={{ left: "5%" }}>
+									{" "}
+									Last accessed: 01/12/2024{" "}
+								</Text>
+							</View>
+						</TouchableOpacity>
+
+						<TouchableOpacity style={styles.profitsListButtons}>
+							<Text style={{ left: "5%" }}> 2023 | 12 items</Text>
+							<View
+								style={{
+									backgroundColor: "#DEF1CA",
+									height: "5%",
+								}}
+							></View>
+							<Text style={{ left: "5%" }}>
+								{" "}
+								Last accessed: 12/21/2022{" "}
+							</Text>
+						</TouchableOpacity>
+					</View>
+					<Button title="View all" color="gray"></Button>
+				</View>
+			</ScrollView>
+
+			<Footer />
+		</View>
+	);
+};
+
+export default ProfitTracking;
